2a54f2fa5a56069b73303dbe2386b8fe1b3500ad
Macallan Savett <jsavett@gmail.com>
2020-04-30 12:53:35 -0700

    get combs and workerbees to render their individual pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'worker_bees/index'
-  root to: 'worker_bees#index'
+  # get 'worker_bees/index', to: 'worker_bees#index'
+  # get 'worker_bees/:id', to: 'worker_bees#show'
+
+# namespace :api, defaults: {format: :json} do
   resources :worker_bees, only: [:index, :show, :update]
   resources :pollen_collections, only: [:create, :destroy]
   resources :supervisor_bees, only: [:create, :destroy]
   resources :combs, only: [:index, :show, :update]
+# end
+root 'worker_bees#index'
 end
