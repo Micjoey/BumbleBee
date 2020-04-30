1a59662051764129bcbac1146f03cd400356fc3d
Macallan Savett <jsavett@gmail.com>
2020-04-30 11:42:31 -0700

    reset migration

--- db/migrate/20200430174957_all_tables_one_migration.rb
+++ db/migrate/20200430174957_all_tables_one_migration.rb
@@ -1,7 +1,8 @@
 class AllTablesOneMigration < ActiveRecord::Migration[5.2]
   def change
     create_table :worker_bees do |t|
-      t.integer "pollen_globs"
+      t.integer "pollen_globs",
+      # t.integer "pollen_globs", array: true, default: []
       t.integer "nectar"
       t.string "bee_name"
       t.integer "comb_id"
@@ -9,9 +10,11 @@ class AllTablesOneMigration < ActiveRecord::Migration[5.2]
     end
     add_index :worker_bees, :pollen_globs
     add_index :worker_bees, :nectar
+    
 
     create_table :combs do |t|
-      t.integer "nectar_volume"
+      t.integer "total_nectar_volume", null: false
+      t.integer "nectar_sweetspot", null: false
       t.integer "bee_id"
       t.integer "supervisor_bee_id", null: false
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,40 +9,40 @@
 
 ActiveRecord::Base.transaction do
 
-    WorkerBee.destroy_all
-    Comb.destroy_all
-    SupervisorBee.destroy_all
+    # WorkerBee.destroy_all
+    # Comb.destroy_all
+    # SupervisorBee.destroy_all
 
     require 'faker'
     include Faker
 
-    i = 1
-    40.times do 
-       bee = WorkerBee.create(
-            bee_name: Faker::Name.first_name,
-            pollen_globs: rand(1..30),
-            nectar: rand(1..30),
-            comb_id: i
-        )
-    i += 1
-    end
+    # i = 1
+    # 40.times do 
+    #    bee = WorkerBee.create(
+    #         bee_name: Faker::Name.first_name,
+    #         pollen_globs: rand(1..30),
+    #         nectar: rand(1..30),
+    #         comb_id: i
+    #     )
+    # i += 1
+    # end
 
-    x = 1
-    40.times do 
-       comb = Comb.create(
-            bee_id: x-1,
-            supervisor_bee_id: x
-        )
-        SupervisorBee.create(
-            bee_id: x,
-            comb_id: x
-        )
-    x += 1
-    end
+    # x = 1
+    # 40.times do 
+    #    comb = Comb.create(
+    #         bee_id: x-1,
+    #         supervisor_bee_id: x
+    #     )
+    #     SupervisorBee.create(
+    #         bee_id: x,
+    #         comb_id: x
+    #     )
+    # x += 1
+    # end
 
-    40.times do
+    # 40.times do
         
 
-    end
+    # end
    
 end
