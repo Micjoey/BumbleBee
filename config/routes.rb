87882eed41ee486a5a1eb238d0210d0327fabcc7
Macallan Savett <jsavett@gmail.com>
2020-05-01 18:29:51 -0700

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  # get 'worker_bees/index', to: 'worker_bees#index'
-  # get 'worker_bees/:id', to: 'worker_bees#show'
 
-# namespace :api, defaults: {format: :json} do
   resources :worker_bees, except: [:destroy]
   resources :pollen_collections, only: [:create, :destroy]
   resources :supervisor_bees, only: [:create, :destroy]
-  resources :combs, only: [:index, :show, :update]
-# end
+  # resources :combs, only: [:index, :show, :update]
+
 root 'worker_bees#index'
 end
